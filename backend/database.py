d2d44a6e01345bf835d47ad350ec38cb6877f210
Shiva <vedayyagari12@gmail.com>
2026-04-03 14:51:47 -0400

    fix database url to use env variable

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Smera0713!@localhost:5432/ai_fitness_db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
